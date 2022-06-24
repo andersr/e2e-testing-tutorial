864f98ef0db19538efe112fa070d3d87f50935c5
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2022-06-24 09:59:30 -0400

    feat: :sparkles: Add newsletter form with e2e tests

---
+++ src/pages/api/newsletter.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import emailValidator from "email-validator";
+
+export type ServerResponse = {
+  message: string;
+  success: boolean;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ServerResponse>
+) {
+  const { email } = req.body;
+
+  if (!email) {
+    res.status(400).json({ message: "Email is required", success: false });
+    return;
+  }
+
+  if (!emailValidator.validate(email)) {
+    res
+      .status(400)
+      .json({ message: "Not a validly formed email address", success: false });
+    return;
+  }
+
+  res.status(200).json({
+    message: `"${email}" was added to the mailing list. Thanks for signing up!`,
+    success: true,
+  });
+}
