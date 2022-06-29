3eb840f2c46d5ff59150dc7568f30c25b0c94805
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2022-06-29 15:06:18 -0400

    Get remaining tests to pass

--- cypress/e2e/newsletterSignup.cy.ts
+++ cypress/e2e/newsletterSignup.cy.ts
@@ -23,55 +23,37 @@ describe("Newsletter signup", () => {
     cy.get('[data-test="newsletter"]').contains("Sign up for our newsletter");
   });
 
-  it("allows for completing a newsletter signup");
+  it("allows for completing a newsletter signup", () => {
+    cy.intercept("POST", "/api/newsletter", {
+      statusCode: 200,
+      body: {
+        success: true,
+        message: "Success message",
+      },
+    });
 
-  it("displays a success message on successful form submit");
+    cy.get('[data-test="emailInput"]').type("test@email.com");
+    cy.get('[data-test="formSubmit"]').click();
+    cy.get('[data-test="successMessage"]').should("exist");
+  });
 
-  it("clears the email input if signup is successful");
+  it("displays an error message if input value is not a valid email address", () => {
+    cy.get('[data-test="emailInput"]').type(`${INVALID_EMAIL}{enter}`);
+    cy.get('[data-test="errorMessage"]').should("exist");
+  });
 
-  it("allows for submitting the form by using the enter key");
+  it("displays an error message if the input field is empty", () => {
+    cy.get('[data-test="emailInput"]').type("{enter}");
+    cy.get('[data-test="errorMessage"]').should("exist");
+  });
 
-  it("displays an error message if input value is not a valid email address");
+  it("clears the email input if signup is successful", () => {
+    successfulFormSubmit();
+    cy.get("#emailInput").should("have.value", "");
+  });
 
-  it("displays an error message if the input field is empty");
-
-  it("persists the email input value if there is an error");
+  it("persists the email input value if there is an error", () => {
+    cy.get('[data-test="emailInput"]').type(`${INVALID_EMAIL}{enter}`);
+    cy.get("#emailInput").should("have.value", INVALID_EMAIL);
+  });
 });
-
-// it("allows for completing a newsletter signup", () => {
-//   successfulFormSubmit();
-//   cy.get('[data-test="successMessage"]').should("exist");
-// });
-
-// it("allows for submitting the form by using the enter key", () => {
-//   cy.intercept("POST", "/api/newsletter", {
-//     statusCode: 200,
-//     body: {
-//       success: true,
-//       message: "Success message",
-//     },
-//   });
-
-//   cy.get('[data-test="emailInput"]').type("test@email.com{enter}");
-//   cy.get('[data-test="successMessage"]').should("exist");
-// });
-
-// it("displays an error message if input value is not a valid email address", () => {
-//   cy.get('[data-test="emailInput"]').type(`${INVALID_EMAIL}{enter}`);
-//   cy.get('[data-test="errorMessage"]').should("exist");
-// });
-
-// it("displays an error message if the input field is empty", () => {
-//   cy.get('[data-test="emailInput"]').type("{enter}");
-//   cy.get('[data-test="errorMessage"]').should("exist");
-// });
-
-// it("clears the email input if signup is successful", () => {
-//   successfulFormSubmit();
-//   cy.get("#emailInput").should("have.value", "");
-// });
-
-// it("persists the email input value if there is an error", () => {
-//   cy.get('[data-test="emailInput"]').type(`${INVALID_EMAIL}{enter}`);
-//   cy.get("#emailInput").should("have.value", INVALID_EMAIL);
-// });
